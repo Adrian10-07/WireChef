50e83cda652f905604484563cd428b3ef18cf002
Adrian10-07 <adrian2aguilarg@gmail.com>
2026-02-25 04:00:57 -0600

    data user

---
+++ app/src/main/java/com/example/wirechef/features/WireChef/data/datasource/remote/mapper/user/UserMapper.kt
@@ -0,0 +1,12 @@
+package com.example.wirechef.features.WireChef.data.datasource.remote.mapper.user
+
+import com.example.wirechef.features.WireChef.data.datasource.remote.models.user.UserDto
+import com.example.wirechef.features.WireChef.domain.entities.
+
+fun UserDto.toDomain(): User {
+    return User(
+        id = this.id,
+        name = this.name,
+        role = this.role
+    )
+}
